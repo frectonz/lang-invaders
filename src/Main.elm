325639564b8a060aa35efd5efe7f970eb5b39a35
frectonz <fraol0912@gmail.com>
2023-01-11 00:56:12 +0300

    enemy movement

--- src/Main.elm
+++ src/Main.elm
@@ -3,10 +3,11 @@ module Main exposing (main)
 import Browser exposing (Document)
 import Browser.Dom exposing (Viewport, getViewport)
 import Browser.Events exposing (onKeyDown, onResize)
-import Html exposing (div)
-import Html.Attributes exposing (class, style)
 import Json.Decode as Decode exposing (Decoder)
+import Svg exposing (g, rect, svg)
+import Svg.Attributes exposing (fill, height, viewBox, width, x, y)
 import Task
+import Time
 
 
 main : Program () Model Msg
@@ -22,6 +23,9 @@ main =
 type alias Model =
     { player : Object
     , scene : Scene
+    , enemies : List Object
+    , enemiesPos : { x : Float, y : Float }
+    , enemiesDelta : { x : Float, y : Float }
     }
 
 
@@ -39,10 +43,54 @@ type alias Scene =
     }
 
 
+enemiesGap : Float
+enemiesGap =
+    10
+
+
+enemySize : Float
+enemySize =
+    50
+
+
+rows : Int
+rows =
+    5
+
+
+cols : Int
+cols =
+    11
+
+
+enemiesGroupWidth : Float
+enemiesGroupWidth =
+    toFloat cols * (enemySize + enemiesGap)
+
+
 init : () -> ( Model, Cmd Msg )
 init _ =
     ( { player = { x = 0, y = 0, width = 100, height = 30 }
       , scene = { width = 0, height = 0 }
+      , enemies =
+            List.repeat (rows * cols) 0
+                |> List.indexedMap
+                    (\i _ ->
+                        let
+                            row =
+                                i // cols
+
+                            col =
+                                i - (row * cols)
+                        in
+                        { x = toFloat col * (enemySize + enemiesGap)
+                        , y = toFloat row * (enemySize + enemiesGap)
+                        , width = enemySize
+                        , height = enemySize
+                        }
+                    )
+      , enemiesPos = { x = 0, y = 0 }
+      , enemiesDelta = { x = 20, y = 20 }
       }
     , Task.perform GotViewPort getViewport
     )
@@ -53,10 +101,11 @@ type Msg
     | GotViewPort Viewport
     | SceneResize Int Int
     | MovePlayer Direction
+    | MoveEnemies Time.Posix
 
 
-padding : Float
-padding =
+scenePadding : Float
+scenePadding =
     100
 
 
@@ -68,30 +117,49 @@ update msg model =
 
         GotViewPort { scene } ->
             let
+                newScene =
+                    { width = scene.width - scenePadding
+                    , height = scene.height - scenePadding
+                    }
+
                 player =
                     model.player
+
+                newPlayer =
+                    { player
+                        | y = newScene.height - player.height
+                    }
             in
             ( { model
-                | scene = scene
-                , player = { player | x = padding, y = scene.height - padding - player.height }
+                | scene = newScene
+                , player = newPlayer
               }
             , Cmd.none
             )
 
         SceneResize w h ->
             let
-                newWidth =
-                    toFloat w
+                scene =
+                    model.scene
 
-                newHeight =
-                    toFloat h
+                newScene =
+                    { scene
+                        | width = toFloat w - scenePadding
+                        , height = toFloat h - scenePadding
+                    }
 
                 player =
                     model.player
+
+                newPlayer =
+                    { player
+                        | y = newScene.height - player.height
+                        , x = 0
+                    }
             in
             ( { model
-                | scene = { width = newWidth, height = newHeight }
-                , player = { player | x = padding, y = newHeight - padding - player.height }
+                | scene = newScene
+                , player = newPlayer
               }
             , Cmd.none
             )
@@ -100,6 +168,9 @@ update msg model =
             let
                 player =
                     model.player
+
+                scene =
+                    model.scene
             in
             case direction of
                 Left ->
@@ -108,8 +179,8 @@ update msg model =
                             player.x - 10
 
                         newX =
-                            if calculatedX < padding then
-                                padding
+                            if calculatedX <= 0 then
+                                0
 
                             else
                                 calculatedX
@@ -122,8 +193,8 @@ update msg model =
                             player.x + 10
 
                         newX =
-                            if calculatedX > model.scene.width - padding - player.width then
-                                model.scene.width - padding - player.width
+                            if calculatedX > scene.width - player.width then
+                                scene.width - player.width
 
                             else
                                 calculatedX
@@ -133,20 +204,80 @@ update msg model =
                 Other ->
                     ( model, Cmd.none )
 
+        MoveEnemies _ ->
+            let
+                enemiesX =
+                    model.enemiesPos.x
+
+                enemiesY =
+                    model.enemiesPos.y
+
+                enemiesDeltaX =
+                    model.enemiesDelta.x
+
+                enemiesDeltaY =
+                    model.enemiesDelta.y
+
+                scene =
+                    model.scene
+
+                newEnemiesX =
+                    enemiesX + model.enemiesDelta.x
+
+                newEnemiesDeltaX =
+                    if newEnemiesX > scene.width - enemiesGroupWidth || newEnemiesX < 0 then
+                        enemiesDeltaX * -1
+
+                    else
+                        enemiesDeltaX
+            in
+            ( { model
+                | enemiesPos =
+                    { x = newEnemiesX
+                    , y = enemiesY
+                    }
+                , enemiesDelta =
+                    { x = newEnemiesDeltaX
+                    , y = enemiesDeltaY
+                    }
+              }
+            , Cmd.none
+            )
+
 
 view : Model -> Document Msg
-view model =
+view { scene, player, enemies, enemiesPos } =
     { title = "Lang Invaders"
     , body =
-        [ div
-            [ class "player"
-            , style "position" "absolute"
-            , style "top" (String.fromFloat model.player.y ++ "px")
-            , style "left" (String.fromFloat model.player.x ++ "px")
-            , style "width" (String.fromFloat model.player.width ++ "px")
-            , style "height" (String.fromFloat model.player.height ++ "px")
+        [ svg
+            [ width (String.fromFloat scene.width)
+            , height (String.fromFloat scene.height)
+            , viewBox ("0 0 " ++ String.fromFloat scene.width ++ " " ++ String.fromFloat scene.height)
             ]
-            []
+            [ g
+                []
+                (enemies
+                    |> List.map
+                        (\e ->
+                            rect
+                                [ e.x + enemiesPos.x |> String.fromFloat |> x
+                                , e.y + enemiesPos.y |> String.fromFloat |> y
+                                , e.width |> String.fromFloat |> width
+                                , e.height |> String.fromFloat |> height
+                                , fill "red"
+                                ]
+                                []
+                        )
+                )
+            , rect
+                [ player.x |> String.fromFloat |> x
+                , player.y |> String.fromFloat |> y
+                , player.width |> String.fromFloat |> width
+                , player.height |> String.fromFloat |> height
+                , fill "white"
+                ]
+                []
+            ]
         ]
     }
 
@@ -180,4 +311,5 @@ subscriptions _ =
     Sub.batch
         [ onResize SceneResize
         , onKeyDown keyDecoder |> Sub.map MovePlayer
+        , Time.every 100 MoveEnemies
         ]
